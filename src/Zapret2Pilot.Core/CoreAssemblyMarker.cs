ede98b80d344e7bb3e9a5dc4d1847dd9499837cb
MrFr3di <ynagi-wm@mail.ru>
2026-06-16 13:55:23 +0500

    chore: initialize .NET solution foundation

---
+++ src/Zapret2Pilot.Core/CoreAssemblyMarker.cs
@@ -0,0 +1,12 @@
+namespace Zapret2Pilot.Core;
+
+/// <summary>
+/// Marker for the Zapret2Pilot.Core assembly.
+/// </summary>
+public static class CoreAssemblyMarker
+{
+    public const string ProductName = "Zapret2Pilot";
+    public const string ShortName = "Z2P";
+    public const string MainExecutableName = "z2p.exe";
+    public const string LayerName = "Core";
+}
