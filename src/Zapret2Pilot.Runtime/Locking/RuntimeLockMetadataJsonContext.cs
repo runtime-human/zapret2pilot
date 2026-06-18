addd8aafbd620f90c86d78911beb7e2ee576df5c
MrFr3di <ynagi-wm@mail.ru>
2026-06-18 17:04:48 +0500

    fix(runtime): include process metadata json type

--- src/Zapret2Pilot.Runtime/Locking/RuntimeLockMetadataJsonContext.cs
+++ src/Zapret2Pilot.Runtime/Locking/RuntimeLockMetadataJsonContext.cs
@@ -4,6 +4,7 @@ namespace Zapret2Pilot.Runtime.Locking;
 
 [JsonSourceGenerationOptions(WriteIndented = true)]
 [JsonSerializable(typeof(RuntimeLockMetadata))]
+[JsonSerializable(typeof(RuntimeLockProcessMetadata))]
 internal sealed partial class RuntimeLockMetadataJsonContext : JsonSerializerContext
 {
 }
