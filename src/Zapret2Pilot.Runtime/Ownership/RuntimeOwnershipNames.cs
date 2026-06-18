4838d30b94d5e4443da32a0a53f4d2f546dfe68c
MrFr3di <ynagi-wm@mail.ru>
2026-06-18 15:13:30 +0500

    feat(runtime): add runtime ownership names

---
+++ src/Zapret2Pilot.Runtime/Ownership/RuntimeOwnershipNames.cs
@@ -0,0 +1,8 @@
+namespace Zapret2Pilot.Runtime.Ownership;
+
+public static class RuntimeOwnershipNames
+{
+    public const string GlobalMutexName = @"Global\Z2P_RUNTIME_OWNER_v1";
+
+    public const string LockFileName = "z2p-runtime.lock";
+}
