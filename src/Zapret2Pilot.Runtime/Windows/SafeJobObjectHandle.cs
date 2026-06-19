aeb718b1bcbe282eea274e78066dce3ddc3007b8
MrFr3di <ynagi-wm@mail.ru>
2026-06-19 16:26:24 +0500

    fix(runtime): make job handle constructors analyzer-safe

--- src/Zapret2Pilot.Runtime/Windows/SafeJobObjectHandle.cs
+++ src/Zapret2Pilot.Runtime/Windows/SafeJobObjectHandle.cs
@@ -5,9 +5,15 @@ namespace Zapret2Pilot.Runtime.Windows;
 
 internal sealed class SafeJobObjectHandle : SafeHandleZeroOrMinusOneIsInvalid
 {
-    private SafeJobObjectHandle()
+    internal SafeJobObjectHandle()
         : base(ownsHandle: true)
     {
+    }
+
+    internal SafeJobObjectHandle(IntPtr handle)
+        : base(ownsHandle: true)
+    {
+        SetHandle(handle);
     }
 
     protected override bool ReleaseHandle()
